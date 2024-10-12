0b58d040c304226a34b80d9689f6e47a96a1373e
Kolapo Wariz <kolapowariz@gmail.com>
2024-10-12 14:04:40 +0100

    learning test chapter 2

--- src/intro.js
+++ src/intro.js
@@ -1,8 +1,6 @@
 // Lesson: Writing your first tests
 export function max(a, b) {
-  if (a > b) return a;
-  else if (b > a) return b;
-  return a;
+  return (a > b ? a : b);
 }
 
 // Exercise
@@ -11,4 +9,20 @@ export function fizzBuzz(n) {
   if (n % 3 === 0) return 'Fizz';
   if (n % 5 === 0) return 'Buzz';
   return n.toString();
+}
+
+
+export function calculateAvg(n){
+  if (n.length === 0) return NaN;
+  const sum = n.reduce((sum, curr) => sum + curr, 0)
+  return sum / n.length;
+}
+
+
+export function factorial(n) {
+  if(n < 0) return undefined;
+  
+  if (n === 0 || n === 1) return 1;
+  
+  return n * factorial(n - 1);
 }
